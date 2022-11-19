cbd8e2e707f83e0521e6ce36adab7790a32e0e02
althafvly <althafvly@gmail.com>
2022-11-19 12:45:23 +0530

    elish: Add libpiex_shim for com.mi.node.watermark.so
    Change-Id: I9e8b125ab5dfaa325db63b02a7fcf51577ee7b0d

--- device.mk
+++ device.mk
@@ -26,6 +26,9 @@ TARGET_SCREEN_WIDTH := 1600
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/camera/camera_cnf.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camera_cnf.txt
 
+PRODUCT_PACKAGES += \
+    libpiex_shim
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
