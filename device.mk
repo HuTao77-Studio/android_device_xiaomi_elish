ed726906cce5f08109d8085029ca08f3ffa9b4d1
Fuutao <fuutaoao@gmail.com>
2024-03-09 15:57:34 +0800

    elish: rro_overlays: Use MIUI captive portal configs
    Signed-off-by: Fuutao <fuutaoao@gmail.com>

--- device.mk
+++ device.mk
@@ -66,6 +66,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+    NetworkStackOverlayElish
+
 # Peripheral Manager
 PRODUCT_PACKAGES += \
     XiaomiPeripheralManager
