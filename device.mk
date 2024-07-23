e3d770a33e97649e61e600ca532fa75747d92d68
Fuutao <fuutaoao@gmail.com>
2024-07-23 15:13:09 +0800

    elish: overlay-lineage: Configure aux camera for Aperture
    Signed-off-by: Fuutao <fuutaoao@gmail.com>

--- device.mk
+++ device.mk
@@ -63,7 +63,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Peripheral Manager
 PRODUCT_PACKAGES += \
